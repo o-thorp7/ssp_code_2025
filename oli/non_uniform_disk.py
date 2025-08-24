2703137a0e4cb18e4d412b434078cce808a6ef10
Oliver Thorp <o_thorp@Olivers-MacBook-Air.local>
2025-08-24 17:54:49 +1000

    unsuccessful 0 spike fix

--- oli/non_uniform_disk.py
+++ oli/non_uniform_disk.py
@@ -2,60 +2,86 @@ import numpy as np, matplotlib.pyplot as plt, math
 from scipy.ndimage import gaussian_filter1d
 
 # Constants
-RADIUS = 10 # metres
+INNER_RAD = 0.1 # r1
+OUTER_RAD = 10  # r2
 DISK_RES = 200 # pixels per metre
-PIX_RADIUS = RADIUS * DISK_RES
-NUM_BINS = int(56 * PIX_RADIUS ** (1/3)) # Freedman rule
-U_MAX = 5.0 / math.sqrt(RADIUS)
+PIX_RaDIUS = OUTER_RAD * DISK_RES
+NUM_BINS = int(56 * PIX_RaDIUS ** (1/3)) # Freedman rule
+U_MAX = 5.0 / math.sqrt(OUTER_RAD)
 BIN_WIDTH = 2 * U_MAX / NUM_BINS
-ALPHA = 0.4
+ALPHA = 1.5
 
 def get_u(x, y):
-    r2 = x*x + y*y
+    r2 = x**2 + y**2
     if r2 == 0.0:
+        return math.inf
+    # attempt to fix mask:
+    # set u = inf for small radius so that it is excluded from the plot
+    if r2 < INNER_RAD**2:
         return math.inf
     return x * (r2 ** -0.75)
 
-def get_brightness(x: float, y: float) -> float:
+def get_brightness_shifted(x: float, y: float) -> float:
     r = math.sqrt(x**2 + y**2)
-    r_dash = (r + RADIUS) / RADIUS
-    if (r > RADIUS):
+    r_dash = (r + OUTER_RAD) / OUTER_RAD
+    if (r > OUTER_RAD):
         return 0.0
     
     # Option 0: smiley face :)
-    # if (
-    #     (x-RADIUS/3)**2 + (-y-RADIUS/2)**2 <= (RADIUS**2)/35 or
-    #     (x- 2*RADIUS/3)**2 + (-y-RADIUS/2)**2 <= (RADIUS**2)/35 or
-    #     (
-    #         (-y <= RADIUS/8) and 
-    #         ((x-RADIUS/2)**2 + (-y-RADIUS/8)**2 <= (RADIUS**2)/8) and
-    #         ((x-RADIUS/2)**2 + (-y-RADIUS/8)**2 >= (RADIUS**2)/10)
-    #     )
-    # ):
-    #     return 1
-    # return 0.5
+    if (
+        (x-OUTER_RAD/3)**2 + (-y-OUTER_RAD/2)**2 <= (OUTER_RAD**2)/35 or
+        (x- 2*OUTER_RAD/3)**2 + (-y-OUTER_RAD/2)**2 <= (OUTER_RAD**2)/35 or
+        (
+            (-y <= OUTER_RAD/8) and 
+            ((x-OUTER_RAD/2)**2 + (-y-OUTER_RAD/8)**2 <= (OUTER_RAD**2)/8) and
+            ((x-OUTER_RAD/2)**2 + (-y-OUTER_RAD/8)**2 >= (OUTER_RAD**2)/10)
+        )
+    ):
+        return 1
+    return 0.5
 
     # Option 1: constant
     # return 1
 
     # Option 2: linear in x
-    # return (x + 2*RADIUS) / (4*RADIUS)
+    # return (x + 2*OUTER_RAD) / (4*OUTER_RAD)
 
     # Option 3: inverse power law:
     # return (r_dash**(-ALPHA))
 
     # Option 4: combination
-    return (((x + 2*RADIUS) / (4*RADIUS)) + (r_dash**(-ALPHA)))/2
+    # return (((x + 2*OUTER_RAD) / (4*OUTER_RAD)) + (r_dash**(-ALPHA)))/2
 
-def make_brightness_grid() -> list[float, float]:
+def get_brightness(x: float, y: float) -> float:
+    r = math.sqrt(x**2 + y**2)
+    if (r > OUTER_RAD or r < INNER_RAD):
+        return 0.0
+    
+    # Option 1: constant
+    # return 1
+
+    # Option 2: linear in x
+    # return (x + 2*OUTER_RAD) / (4*OUTER_RAD)
+
+    # Option 3: inverse power law:
+    return ((r/INNER_RAD)**(-ALPHA))
+
+    # Option 4: combination
+    # return (((x + 2*OUTER_RAD) / (4*OUTER_RAD)) + (r_dash**(-ALPHA)))/2
+
+
+def make_brightness_grid(is_shifted: bool = False) -> list[float, float]:
     # each coordinate represents the corresponding brightness coordinate on the disk
-    brightness_grid = np.zeros((PIX_RADIUS*2, PIX_RADIUS*2))
+    brightness_grid = np.zeros((PIX_RaDIUS*2, PIX_RaDIUS*2))
 
-    for i in range(-PIX_RADIUS, PIX_RADIUS):
-        for j in range(-PIX_RADIUS, PIX_RADIUS):
+    for i in range(-PIX_RaDIUS, PIX_RaDIUS):
+        for j in range(-PIX_RaDIUS, PIX_RaDIUS):
             x = j / DISK_RES
             y = i / DISK_RES
-            brightness_grid[i+PIX_RADIUS, j+PIX_RADIUS] = get_brightness(x, y)
+            if is_shifted:
+                brightness_grid[i+PIX_RaDIUS, j+PIX_RaDIUS] = get_brightness_shifted(x, y)
+            else:
+                brightness_grid[i+PIX_RaDIUS, j+PIX_RaDIUS] = get_brightness(x, y)
 
     return brightness_grid
 
@@ -78,16 +104,16 @@ def make_u_intensities(brightness_grid: list[float, float]) -> list[float]:
 
     u_intensities = np.zeros((NUM_BINS, 2))
     u_intensities[:, 0] = bin_starts
-    for i in range(-PIX_RADIUS, PIX_RADIUS):
-        for j in range(-PIX_RADIUS, PIX_RADIUS):
+    for i in range(-PIX_RaDIUS, PIX_RaDIUS):
+        for j in range(-PIX_RaDIUS, PIX_RaDIUS):
             x = j / DISK_RES
             y = i / DISK_RES
-            if (x**2 + y**2 > RADIUS**2):
+            if (x**2 + y**2 > OUTER_RAD**2):
                 continue
             u = get_u(x, y)
             if abs(u) > U_MAX:
                 continue
-            brightness = brightness_grid[i+PIX_RADIUS, j+PIX_RADIUS]
+            brightness = brightness_grid[i+PIX_RaDIUS, j+PIX_RaDIUS]
             bin_index = int((u + U_MAX) / BIN_WIDTH)
             if bin_index < 0 or bin_index >= NUM_BINS:
                 continue
@@ -97,6 +123,9 @@ def make_u_intensities(brightness_grid: list[float, float]) -> list[float]:
 
 def plot_u_intensities(u_intensities: list[float, float]) -> None:
     bin_starts = np.linspace(-U_MAX, U_MAX, NUM_BINS, endpoint=False)
+    #TODO: remove
+    # plt.xkcd()
+    #
     plt.plot(bin_starts, u_intensities[:, 1])
     plt.grid(True)
     plt.show()
@@ -104,9 +133,9 @@ def plot_u_intensities(u_intensities: list[float, float]) -> None:
 def flatten_spike(u_intens: list[float], n: int) -> list[float]:
     # average the middle n bins
     indices = np.arange(int((NUM_BINS-n)/2), int((NUM_BINS+n)/2), 1)
-    print(f"middle bit: {u_intens[indices,1]}")
+    # print(f"middle bit: {u_intens[indices,1]}")
     av = np.mean(u_intens[indices, 1])
-    print(f"indices, av: {indices}, {av}")
+    # print(f"indices, av: {indices}, {av}")
     flattened = np.copy(u_intens)
     for i in indices:
         flattened[i] = av
@@ -119,19 +148,38 @@ def perf_gauss_smooth(u_intens: list[float], sigma: float) -> list[float]:
     smoothed_intens[:,1] = new_brightness
     return smoothed_intens
 
-bg = make_brightness_grid()
+# bg = make_brightness_grid(is_shifted=False)
 # generate_disk_img(bg, adjust_contrast=True)
+
+
+# u_intens = make_u_intensities(bg)
+# print(u_intens[int(NUM_BINS/2) - 2:int(NUM_BINS/2) + 3])
+# plot_u_intensities(u_intens)
+
+# s_u_intens = perf_gauss_smooth(u_intens, sigma=5)
+# print(u_intens[int(NUM_BINS/2) - 2:int(NUM_BINS/2) + 3])
+# plot_u_intensities(s_u_intens)
+
+
+# flattened_u_i = flatten_spike(u_intens, 4)
+# print(flattened_u_i[int(NUM_BINS/2) - 2:int(NUM_BINS/2) + 3])
+# plot_u_intensities(flattened_u_i)
+
+
+
+bg = make_brightness_grid(is_shifted=True)
+# generate_disk_img(bg, adjust_contrast=False)
 
 
 u_intens = make_u_intensities(bg)
 # print(u_intens[int(NUM_BINS/2) - 2:int(NUM_BINS/2) + 3])
 plot_u_intensities(u_intens)
 
-s_u_intens = perf_gauss_smooth(u_intens, 15)
+# s_u_intens = perf_gauss_smooth(u_intens, sigma=5)
 # print(u_intens[int(NUM_BINS/2) - 2:int(NUM_BINS/2) + 3])
-plot_u_intensities(s_u_intens)
+# plot_u_intensities(s_u_intens)
 
 
-# flattened_u_i = flatten_spike(u_intens, 2)
+# flattened_u_i = flatten_spike(u_intens, 4)
 # print(flattened_u_i[int(NUM_BINS/2) - 2:int(NUM_BINS/2) + 3])
 # plot_u_intensities(flattened_u_i)
